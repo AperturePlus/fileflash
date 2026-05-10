1ad208a7e6582ad2164960bab998f4c200c94d86
0neStep <146049978+AperturePlus@users.noreply.github.com>
2026-05-10 23:27:09 +0800

    feat(auth): add profile and startup checks

--- app/src/routers/__init__.py
+++ app/src/routers/__init__.py
@@ -7,6 +7,7 @@ from .jobs import router as jobs_router
 from .me import router as me_router
 from .recycle import router as recycle_router
 from .shares import router as shares_router
+from .storage import router as storage_router
 from .uploads import router as uploads_router
 from .agent_skills import router as agent_skills_router
 
@@ -18,6 +19,7 @@ api_router.include_router(jobs_router)
 api_router.include_router(me_router)
 api_router.include_router(recycle_router)
 api_router.include_router(shares_router)
+api_router.include_router(storage_router)
 api_router.include_router(uploads_router)
 api_router.include_router(agent_skills_router)
 
